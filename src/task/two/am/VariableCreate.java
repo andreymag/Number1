000f91bf1e6240f73f78c24857a336fc3357947a
Andrey <magsumov52@gmail.com>
2021-01-26 23:12:04 +0300

    Второе задание

--- src/task/two/am/VariableCreate.java
+++ src/task/two/am/VariableCreate.java
@@ -6,7 +6,5 @@ public class VariableCreate {
     public static void main(String[] args) {
         double  a = 152.152;
         byte b = 127;
-        /*System.out.println(a);
-        System.out.println(b);*/
     }
 }
