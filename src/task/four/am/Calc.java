068d6f07e18621d761fa5207053928fc602332e2
Andrey <magsumov52@gmail.com>
2021-02-16 00:25:13 +0300

    Четвертое задание

---
+++ src/task/four/am/Calc.java
@@ -0,0 +1,26 @@
+package task.four.am;
+
+
+
+
+import java.util.Scanner;
+public class Calc {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input a number: ");
+        double x = in.nextDouble();
+        double y = in.nextDouble();
+        double z = in.nextDouble();
+        double average;
+        average = (x + y + z) / 3;
+        System.out.println(average);
+        int result = (int)Math.floor(average / 2);
+        //проверка округления среднего арифметического делённого на 2
+        System.out.println(result);
+        if (result > 3){
+            System.out.println("Программа выполнена корректно");
+        }
+
+
+    }
+}
