fbec08dd9fa20e832d1cf07e0126793fc67cb4a0
Andrey <magsumov52@gmail.com>
2021-02-17 03:58:31 +0300

    Пятое задание

---
+++ src/task/five/am/Calculator.java
@@ -0,0 +1,30 @@
+package task.five.am;
+
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        double x = in.nextDouble();
+        System.out.println("Введите второе число: ");
+        double y = in.nextDouble();
+        System.out.println("Введите один из символов '+', '-', '*' или '/'");
+        String z = in.next();
+        switch (z) {
+            case "+":
+                System.out.println("Равно: " + (x + y));
+                break;
+            case "-":
+                System.out.println("Равно: " + (x - y));
+                break;
+            case "*":
+                System.out.println("Равно: " + (x * y));
+                break;
+            default:
+                System.out.println("Равно: " + (x / y));
+                break;
+        }
+    }
+}
