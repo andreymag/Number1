b01bc1d89868848c00b150768b09e0f2851d2536
Andrey <magsumov52@gmail.com>
2021-02-19 03:06:24 +0300

    Десятое задание

---
+++ src/task/ten/am/CycleFour.java
@@ -0,0 +1,26 @@
+package task.ten.am;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CycleFour {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите размер матрицы: ");
+        int size1 = in.nextInt();
+        int size2 = in.nextInt();
+        int[][] array = new int[size1][size2];
+        System.out.println("Введите данные в матрицу: ");
+        for (int i = 0; i < size1; i++) {
+            for(int j=0; j<array[i].length;j++){
+                array[i][j] = in.nextInt();
+            }
+        }
+        int[] row = array [0];
+        for(int s = 0 ; s < row.length ; s++) {
+            row[s] *= 3;
+        }
+        System.out.println(Arrays.toString(row));
+
+    }
+}
