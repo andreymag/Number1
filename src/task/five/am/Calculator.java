81cc8d87842bb26506ad17b7bd03722496525cf1
Andrey <magsumov52@gmail.com>
2021-02-25 19:30:22 +0300

    Пятое задание + проверка деления на 0

--- src/task/five/am/Calculator.java
+++ src/task/five/am/Calculator.java
@@ -12,19 +12,24 @@ public class Calculator {
         double y = in.nextDouble();
         System.out.println("Введите один из символов '+', '-', '*' или '/'");
         String z = in.next();
-        switch (z) {
-            case "+":
-                System.out.println("Равно: " + (x + y));
-                break;
-            case "-":
-                System.out.println("Равно: " + (x - y));
-                break;
-            case "*":
-                System.out.println("Равно: " + (x * y));
-                break;
-            default:
-                System.out.println("Равно: " + (x / y));
-                break;
+        if (y == 0 && z.equals("/")){
+            System.out.println("Делить на ноль нельзя");
+        }
+        else {
+            switch (z) {
+                case "+":
+                    System.out.println("Равно: " + (x + y));
+                    break;
+                case "-":
+                    System.out.println("Равно: " + (x - y));
+                    break;
+                case "*":
+                    System.out.println("Равно: " + (x * y));
+                    break;
+                default:
+                    System.out.println("Равно: " + (x / y));
+                    break;
+            }
         }
     }
 }
