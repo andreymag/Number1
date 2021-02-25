c5e902f71dfad9c517fb39a17b6cb3154873367c
Andrey <magsumov52@gmail.com>
2021-02-26 00:40:23 +0300

    Тринадцатое задание

---
+++ src/task/thirteen/am/DataWorkThree.java
@@ -0,0 +1,32 @@
+package task.thirteen.am;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DataWorkThree {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите слова одной строкой через пробел");
+        String slova1 = in.nextLine();
+        boolean check1 = slova1.contains(" ");
+        if (check1){
+            Pattern latin = Pattern.compile("\\b[A-Za-z]+\\b");
+            Matcher m = latin.matcher(slova1);
+            StringBuilder slova2 = new StringBuilder();
+            while (m.find()) {
+                slova2.append(m.group()).append(" ");
+            }
+            System.out.println(slova2);
+            int count = 0;
+            for (int i = 0; i < slova2.length(); i++) {
+                if(slova2.charAt(i) == ' '){
+                    count++;
+                }
+            }
+            System.out.println("Кол-во слов с латинецей "+count);
+        }
+        else
+            System.out.println("Вы ввели одно слово");
+    }
+}
