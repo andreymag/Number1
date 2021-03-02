343244e298e9c44f598deb9920165243354a95cb
Andrey <magsumov52@gmail.com>
2021-03-02 10:05:19 +0300

    Итоговая задача №4

--- src/task/finalFour/am/FinalFour.java
+++ src/task/finalFour/am/FinalFour.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class FinalFour {
     public static void main(String[] args) {
-        System.out.println("Отгадайте загадку, у вас есть 3 попытки, и право на подсказку при первой попытке, если вы воспользовались подсказкой, то 2 и 3 попытки станут недоступны.");
-        System.out.println("Загадка: \"Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает\".");
+        System.out.println("Отгадайте загадку!!! У вас есть 3 попытки, и право на подсказку при первой попытке." +
+                "\nЕсли вы воспользовались подсказкой, то 2 и 3 попытки станут недоступны." +
+                "\nЗагадка: \"Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает\"." +
+                "\nЕсли вы хотите воспользоваться подсказкой введите: Подсказка.");
         String trueAnswer = "Заархивированный вирус";
         String podskazka = "Подсказка";
         String noPodskazka = "Подсказка уже недоступна!";
@@ -13,7 +15,7 @@ public class FinalFour {
         String noPravilno ="Обидно, приходи в другой раз!";
         String falseAnswer = "Подумай еще!";
         Scanner in = new Scanner(System.in);
-        System.out.print("Попытка № 1, если вы хотите воспользоваться подсказкой введите \"Подсказка\": ");
+        System.out.print("Попытка № 1: ");
         String answer1 = in.nextLine();
         if (answer1.equalsIgnoreCase(trueAnswer)){
             System.out.println(pravilno);
@@ -48,12 +50,8 @@ public class FinalFour {
                         System.out.println(noPodskazka);
                         System.out.print("Попытка № 3: ");
                         answer1 = in.nextLine();
-                        if (answer1.equalsIgnoreCase(trueAnswer)) {
-                            System.out.println(pravilno);
-                        } else {
-                            System.out.println(noPravilno);
-                        }
-                    } else if (answer1.equalsIgnoreCase(trueAnswer)) {
+                    }
+                    if (answer1.equalsIgnoreCase(trueAnswer)) {
                         System.out.println(pravilno);
                     } else {
                         System.out.println(noPravilno);
@@ -74,17 +72,11 @@ public class FinalFour {
                     System.out.println(noPodskazka);
                     System.out.print("Попытка № 3: ");
                     answer1 = in.nextLine();
-                    if (answer1.equalsIgnoreCase(trueAnswer)){
-                        System.out.println(pravilno);
-                    }
-                    else {
-                        System.out.println(noPravilno);
-                    }
                 }
-                else if (answer1.equalsIgnoreCase(trueAnswer)){
+                if (answer1.equalsIgnoreCase(trueAnswer)){
                     System.out.println(pravilno);
                 }
-                else{
+                else {
                     System.out.println(noPravilno);
                 }
             }
